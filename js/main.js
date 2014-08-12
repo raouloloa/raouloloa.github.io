9f3c3c4ad7637dfdfc1e169efdf57473ab2930cd
Raoul Oloa <raouloloa@me.com>
2014-08-12 17:45:29 -0500

    cleaning up syntax

--- js/main.js
+++ js/main.js
@@ -6,18 +6,18 @@ var main = function(){
 
         $('body').animate({
             left:'285px'
-        }, 200)
+        }, 200);
     });
 
     $('.icon-close').click(function(){
         $('.menu').animate({
             left:'-285px'
-        },200)
+        },200);
 
         $('body').animate({
             left:'0px'
-        },200)
-    })
-}
+        },200);
+    });
+};
 
 $(document).ready(main);
